4fe0cff031030088a19aaf00bbde986cc4b880f7
YAMASHIRO Yoshihiro <me@xn--uist1idrju3i.jp>
2025-03-11 16:26:20 +0900

    Add SampleCode

--- public_html/codemirror.js
+++ public_html/codemirror.js
@@ -1,7 +1,27 @@
+const defaultRubyCode = `# RGB LED Blinking Example
+while true do
+  # Red
+  LED.set([255, 0, 0])
+  sleep 1
+
+  # Green
+  LED.set([0, 255, 0])
+  sleep 1
+
+  # Blue
+  LED.set([0, 0, 255])
+  sleep 1
+
+  # Check if reload is requested
+  break if Blink.req_reload?
+end
+`;
+
 var editor = CodeMirror.fromTextArea(document.getElementById("ruby-code"), {
   mode: "ruby",
   theme: "dracula",
   lineNumbers: true,
   matchBrackets: true,
   autoCloseBrackets: true,
+  value: defaultRubyCode,
 });
